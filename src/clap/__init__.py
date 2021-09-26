2c616a867e7e83b0950b94f17176389d89d17c46
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-09-26 14:06:44 -0400

    Copy code from bugyi.lib.cli

--- src/clap/__init__.py
+++ src/clap/__init__.py
@@ -6,3 +6,12 @@ A thin wrapper around the standard argparse library.
 __author__ = "Bryan M Bugyi"
 __email__ = "bryanbugyi34@gmail.com"
 __version__ = "0.1.0"
+
+from ._api import (
+    Arguments,
+    MainType,
+    NewCommand,
+    Parser,
+    main_factory,
+    new_command_factory,
+)
--- tests/test_cli.py
+++
@@ -1,8 +0,0 @@
-"""Tests the clap.cli module."""
-
-from clap.cli import main
-
-
-def test_main() -> None:
-    """Tests main() function."""
-    assert main([""]) == 0
