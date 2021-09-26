d4d2ad48f18e6f10e245363be80525c5cf6f939a
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-09-26 16:22:14 -0400

    Use logutils.pytest_plugin instead of duplicating 'mock_dynamic_log_fields'

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,9 +9,11 @@ from _pytest.config import Config
 from _pytest.nodes import Item
 from freezegun import freeze_time
 from pytest import fixture
-from pytest_mock.plugin import MockerFixture
 from typeguard import typechecked
 from typeguard.importhook import install_import_hook
+
+
+pytest_plugins = ["logutils.pytest_plugin"]
 
 
 def pytest_configure(config: Config) -> None:
@@ -48,24 +50,6 @@ def pytest_runtest_call(item: Item) -> None:
     test_func = getattr(item, "obj", None)
     if test_func is not None:
         setattr(item, "obj", typechecked(test_func))
-
-
-@fixture
-def mock_dynamic_log_fields(mocker: MockerFixture) -> None:
-    """Mock dynamic fields that may be contained in log records."""
-    mocker.patch("logutils.api.getpid", return_value=12345)
-
-    frameinfo = mocker.MagicMock()
-    setattr(frameinfo, "function", "fake_function")
-    setattr(frameinfo, "lineno", "123")
-    mocker.patch(
-        "logutils.api.getframeinfo",
-        return_value=frameinfo,
-    )
-
-    mod = mocker.MagicMock()
-    setattr(mod, "__name__", "fake_module")
-    mocker.patch("logutils.api.getmodule", return_value=mod)
 
 
 @fixture(autouse=True, scope="session")
